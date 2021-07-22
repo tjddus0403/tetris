4cf90c983e73e2acb097f7b13ab81eb6acc0ad9e
sungyeon Kim <youn010403@naver.com>
2021-07-22 16:01:37 +0900

    duediugit add cpptet

---
+++ Assign/cpptet/CTetris.h
@@ -0,0 +1,23 @@
+#pragma once
+#include "Tetris.h"
+
+class CTetris : public Tetris {
+    public:
+        static Matrix** setOfCBlockObjects;
+        Matrix iCScreen;
+        Matrix oCScreen; 
+        Matrix currCBlk;
+        CTetris():Tetris(){
+            iCScreen=Matrix(iScreen);
+            oCScreen=Matrix(iCScreen);
+            };
+        CTetris(int Dy, int Dx):Tetris(Dy,Dx){
+            iCScreen=Matrix(iScreen);
+            oCScreen=Matrix(iCScreen);
+        };
+        ~CTetris();
+        static void init(int *setOfBlockArrays[], int MAX_BLK_TYPES, int MAX_BLK_DEGREES);
+        TetrisState accept(char key);
+        void deleteFullLines();
+};
+//Matrix** CTetris::setOfCBlockObjects;
