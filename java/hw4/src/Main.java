e9bc6b9eba7bfa50f4cf922ba883d12a931d2664
sungyeon Kim <youn010403@naver.com>
2021-09-22 01:15:07 +0900

    add

---
+++ java/hw4/src/Main.java
@@ -0,0 +1,216 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Random;
+
+public class Main {
+    private static int[][][][] setOfBlockArrays = { // [7][4][?][?]
+            {
+                    {
+                            { 1, 1 },
+                            { 1, 1 }
+                    },
+                    {
+                            { 1, 1 },
+                            { 1, 1 }
+                    },
+                    {
+                            { 1, 1 },
+                            { 1, 1 }
+                    },
+                    {
+                            { 1, 1 },
+                            { 1, 1 }
+                    }
+            },
+            {
+                    {
+                            {0, 1, 0},
+                            {1, 1, 1},
+                            {0, 0, 0},
+                    },
+                    {
+                            {0, 1, 0},
+                            {0, 1, 1},
+                            {0, 1, 0},
+                    },
+                    {
+                            {0, 0, 0},
+                            {1, 1, 1},
+                            {0, 1, 0},
+                    },
+                    {
+                            {0, 1, 0},
+                            {1, 1, 0},
+                            {0, 1, 0},
+                    },
+            },
+            {
+                    {
+                            {1, 0, 0},
+                            {1, 1, 1},
+                            {0, 0, 0},
+                    },
+                    {
+                            {0, 1, 1},
+                            {0, 1, 0},
+                            {0, 1, 0},
+                    },
+                    {
+                            {0, 0, 0},
+                            {1, 1, 1},
+                            {0, 0, 1},
+                    },
+                    {
+                            {0, 1, 0},
+                            {0, 1, 0},
+                            {1, 1, 0},
+                    },
+            },
+            {
+                    {
+                            {0, 0, 1},
+                            {1, 1, 1},
+                            {0, 0, 0},
+                    },
+                    {
+                            {0, 1, 0},
+                            {0, 1, 0},
+                            {0, 1, 1},
+                    },
+                    {
+                            {0, 0, 0},
+                            {1, 1, 1},
+                            {1, 0, 0},
+                    },
+                    {
+                            {1, 1, 0},
+                            {0, 1, 0},
+                            {0, 1, 0},
+                    },
+            },
+            {
+                    {
+                            {0, 1, 0},
+                            {1, 1, 0},
+                            {1, 0, 0},
+                    },
+                    {
+                            {1, 1, 0},
+                            {0, 1, 1},
+                            {0, 0, 0},
+                    },
+                    {
+                            {0, 1, 0},
+                            {1, 1, 0},
+                            {1, 0, 0},
+                    },
+                    {
+                            {1, 1, 0},
+                            {0, 1, 1},
+                            {0, 0, 0},
+                    },
+            },
+            {
+                    {
+                            {0, 1, 0},
+                            {0, 1, 1},
+                            {0, 0, 1},
+                    },
+                    {
+                            {0, 0, 0},
+                            {0, 1, 1},
+                            {1, 1, 0},
+                    },
+                    {
+                            {0, 1, 0},
+                            {0, 1, 1},
+                            {0, 0, 1},
+                    },
+                    {
+                            {0, 0, 0},
+                            {0, 1, 1},
+                            {1, 1, 0},
+                    },
+            },
+            {
+                    {
+                            {0, 0, 0, 0},
+                            {1, 1, 1, 1},
+                            {0, 0, 0, 0},
+                            {0, 0, 0, 0},
+                    },
+                    {
+                            {0, 1, 0, 0},
+                            {0, 1, 0, 0},
+                            {0, 1, 0, 0},
+                            {0, 1, 0, 0},
+                    },
+                    {
+                            {0, 0, 0, 0},
+                            {1, 1, 1, 1},
+                            {0, 0, 0, 0},
+                            {0, 0, 0, 0},
+                    },
+                    {
+                            {0, 1, 0, 0},
+                            {0, 1, 0, 0},
+                            {0, 1, 0, 0},
+                            {0, 1, 0, 0},
+                    },
+            },
+    }; // end of setOfBlockArrays
+    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static String line = null;
+    private static int nKeys = 0;
+    private static char getKey() throws IOException {
+        char ch;
+        if (nKeys != 0) {
+            ch = line.charAt(line.length() - nKeys);
+            nKeys--;
+            return ch;
+        }
+        do {
+            line = br.readLine();
+            nKeys = line.length();
+        } while (nKeys == 0);
+        ch = line.charAt(0);
+        nKeys--;
+        return ch;
+    }
+    public static void drawMatrix(Matrix m) {
+        int dy = m.get_dy();
+        int dx = m.get_dx();
+        int array[][] = m.get_array();
+        for (int y=0; y < dy; y++) {
+            for (int x=0; x < dx; x++) {
+                if (array[y][x] == 0) System.out.print("□ ");
+                else if (array[y][x] == 1) System.out.print("■ ");
+                else System.out.print("X ");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) throws Exception {
+        char key;
+        TetrisState state;
+        Tetris.init(setOfBlockArrays);
+        Tetris board = new Tetris(15, 10);
+        Random random = new Random();
+        key = (char) ('0' + random.nextInt(7));
+        board.accept(key);
+        drawMatrix(board.oScreen); System.out.println();
+
+        while ((key = getKey()) != 'q') {
+            state = board.accept(key);
+            drawMatrix(board.oScreen); System.out.println();
+            if (state == TetrisState.NewBlock) {
+                key = (char) ('0' + random.nextInt(7));
+                state = board.accept(key);
+                drawMatrix(board.oScreen); System.out.println();
+                if (state == TetrisState.Finished) break; // Game Over!
+            }
+        }
+        System.out.println("Program terminated!");
+    }
+}
